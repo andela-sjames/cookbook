429594a992a2d51665c17dd8820f484a6e28cb3a
andela-sjames <samuel.james@andela.com>
2016-09-19 23:17:54 +0100

    add basic graphql node to application.

--- ingredients/models.py
+++ ingredients/models.py
@@ -16,7 +16,10 @@ class Category(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
-    category = models.ForeignKey(Category, related_name='ingredients')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
 
     def __str__(self):
         return self.name
